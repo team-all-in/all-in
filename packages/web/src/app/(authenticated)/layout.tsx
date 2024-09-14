30e4be3658c851cc5bff453d2697cb82d54d3782
nonrita <2220025@ecc.ac.jp>
2024-09-14 16:50:25 +0900

    change mainコンテンツが配置される場所の高さをheaderの高さから計算し設定

--- packages/web/src/app/(authenticated)/layout.tsx
+++ packages/web/src/app/(authenticated)/layout.tsx
@@ -1,14 +1,19 @@
 import Header from "~/components/common/header";
+import { headerHeight } from "~/components/common/header";
 
 export default function AuthenticatedLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const mainStyle = {
+    height: `calc(100vh - ${headerHeight}px)`
+  }
+
   return (
     <article>
       <Header />
-      <div>{children}</div>
+      <div style={mainStyle}>{children}</div>
     </article>
   );
 }
