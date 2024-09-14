e70f54d64f2a91c957d6100d29d5905fc00e2557
RYU <takahashi.ryunosuke.job@gmail.com>
2024-09-15 00:12:45 +0900

    refactor(web): update setting style

--- packages/web/src/app/(authenticated)/settings/layout.tsx
+++ packages/web/src/app/(authenticated)/settings/layout.tsx
@@ -3,5 +3,5 @@ export default function SettingsLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className='z-0 h-screen bg-white pt-14'>{children}</div>;
+  return <div className='pt-14'>{children}</div>;
 }
