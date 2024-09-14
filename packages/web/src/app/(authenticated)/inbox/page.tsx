df9e2e4a077fa0066d16a0efc5221f6b47e121f0
nonrita <2220025@ecc.ac.jp>
2024-09-15 01:00:34 +0900

    change itemをコンポーネントで置き換え

--- packages/web/src/app/(authenticated)/inbox/page.tsx
+++ packages/web/src/app/(authenticated)/inbox/page.tsx
@@ -1,4 +1,8 @@
+import MessageItem from './components/message-item';
 import { getMessages } from './data/getMessages';
+import DiscordImg from '../../../../public/app-logo/discord.svg'
+import { AppType } from './components/message-item/app-type';
+import { LabelType } from './components/message-item/label-type';
 
 export default async function Inbox() {
   const messages = await getMessages();
@@ -6,37 +10,9 @@ export default async function Inbox() {
 
   return (
     <div className='h-dvh space-y-2 overflow-y-auto p-3 pt-32 sm:pt-14'>
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
-      <div className='h-10 w-full border' />
+      <MessageItem app={AppType.Slack} user={{img: DiscordImg, name: "natanuki"}} labelType={LabelType.High} message='かつて、静かな村に一人の賢者が住んでいました。彼は村の人々から信頼され、いつも困ったときには助けを求められていました。賢者は知恵を持ち、その知恵を使って多くの問題を解決しました。しかし、ある日突然、村に大きな嵐がやってきました。嵐は村の家々を倒し、畑を荒らし、人々は困り果てました。そんな時、村人たちは賢者のもとに集まり、助けを求めました。' />
+      <MessageItem app={AppType.Discord} user={{img: DiscordImg, name: "natanuki"}} labelType={LabelType.Middle} message='かつて、静かな村に一人の賢者が住んでいました。彼は村の人々から信頼され、いつも困ったときには助けを求められていました。賢者は知恵を持ち、その知恵を使って多くの問題を解決しました。しかし、ある日突然、村に大きな嵐がやってきました。嵐は村の家々を倒し、畑を荒らし、人々は困り果てました。そんな時、村人たちは賢者のもとに集まり、助けを求めました。' />
+      <MessageItem app={AppType.GitHub} user={{img: DiscordImg, name: "natanuki"}} labelType={LabelType.Row} message='かつて、静かな村に一人の賢者が住んでいました。彼は村の人々から信頼され、いつも困ったときには助けを求められていました。賢者は知恵を持ち、その知恵を使って多くの問題を解決しました。しかし、ある日突然、村に大きな嵐がやってきました。嵐は村の家々を倒し、畑を荒らし、人々は困り果てました。そんな時、村人たちは賢者のもとに集まり、助けを求めました。' />
     </div>
   );
 }
