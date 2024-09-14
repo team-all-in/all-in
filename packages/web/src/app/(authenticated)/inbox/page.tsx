90679acaa72a119e54902002f39c4d7a9c104e11
RYU <takahashi.ryunosuke.job@gmail.com>
2024-09-15 03:18:39 +0900

    refactor(web): add key to messageItem

--- packages/web/src/app/(authenticated)/inbox/page.tsx
+++ packages/web/src/app/(authenticated)/inbox/page.tsx
@@ -8,11 +8,11 @@ export default async function Inbox() {
   return (
     <div className='h-dvh space-y-2 overflow-y-auto p-3 pt-32 sm:pt-14'>
       <div className='flex'>
-        <div className='bg-black flex-grow w-4 rounded-full'/>
-        <div className='p-3 w-full flex flex-col gap-4'>
-          <h2 className='text-xl font-bold'>2024/09/15</h2>
+        <div className='w-4 flex-grow rounded-full bg-black' />
+        <div className='flex w-full flex-col gap-4 p-3'>
+          <h2 className='font-bold text-xl'>2024/09/15</h2>
           {messages?.map(message => (
-            <MessageItem {...message} />
+            <MessageItem key={message.id} {...message} />
           ))}
         </div>
       </div>
