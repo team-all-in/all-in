e274072c195a5d7ffab25bd2e1b268c9b64608ef
KorRyu3 <146335193+KorRyu3@users.noreply.github.com>
2024-09-14 20:11:36 +0900

    feat: 全パブリックチャンネルにbotを参加させる

--- packages/api/src/slack.py
+++ packages/api/src/slack.py
@@ -4,13 +4,21 @@ import os
 from dotenv import find_dotenv, load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_sdk import WebClient
 
 load_dotenv(find_dotenv())
 
-logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
 
 # ボットトークンとソケットモードハンドラーを使ってアプリを初期化します
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+# @app.event("message")
+# def handle_message_events(body, logger):
+#     logger.info(body)
+
+
 
 user_id = "U07MEPH4AUA"
 
@@ -20,14 +28,19 @@ def message_test(message, say):
     print(message)
     print("-"*40)
     say(f"Hello, <@{message['user']}>! This message's ts(message_id) is {message['ts']}")
+    say(f"{message['text']}")
 
 
-@app.event("message")
-def handle_message_events(body, logger):
-    logger.info(body)
+channels_list = client.conversations_list()
+channel_ids = [channel['id'] for channel in channels_list['channels']]
 
+for channel_id in channel_ids:
+    # 全チャンネルにbotを追加
+    client.conversations_join(channel=channel_id)
+
+# history = client.conversations_history(channel='C07MELMF3TM')
+# print(history)
 
 # アプリを起動します
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
-
