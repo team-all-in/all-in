51b7d8953a91fbc54e6f442dec6a39ff12ebb2fc
nonrita <2220025@ecc.ac.jp>
2024-09-15 02:47:44 +0900

    change スマホの時のレイアウトを変更しました

--- packages/web/src/app/(authenticated)/inbox/components/message-item/index.tsx
+++ packages/web/src/app/(authenticated)/inbox/components/message-item/index.tsx
@@ -13,7 +13,7 @@ const MessageItem: NextPage<Message> = ({app, sender_image, sender_name, content
   const appType: AppProps = AppsProps[app] || defaultAppProps
 
   return (
-    <div className={'flex flex-col gap-3 h-60 sm:h-56 w-full p-4 pr-8 border rounded-3xl relative overflow-hidden ' + appType.itemClass} >
+    <div className={'flex flex-col gap-3  h-72 sm:h-56  w-full p-4 sm:pr-8 border rounded-3xl relative overflow-hidden ' + appType.itemClass} >
       {appType.img && (
         <Image
           src={appType.img}
@@ -21,16 +21,16 @@ const MessageItem: NextPage<Message> = ({app, sender_image, sender_name, content
           className={'absolute h-56 -right-4 -bottom-4 opacity-30 ' + (appType.hasInvert ? ' filter invert ' : '')}
         />
       )}
-      <div className='flex justify-between z-10'>
-        <div className='flex gap-2 items-center'>
-          <Account appImg={appType.img || DiscordImg} sender_image={sender_image} sender_name={sender_name} />
+      <div className='flex flex-col sm:flex-row gap-4 sm:items-center z-10'>
+        <Account appImg={appType.img || DiscordImg} sender_image={sender_image} sender_name={sender_name} />
+        <div className='flex justify-between items-center w-full'>
           <Label priority={priority}/>
+          <p>00:00</p>
         </div>
-        <p>00:00</p>
       </div>
-      <div className='ml-8 flex-grow flex flex-col justify-between text-ellipsis overflow-hidden z-10'>
+      <div className='sm:ml-4 flex-grow flex flex-col justify-between text-ellipsis overflow-hidden z-10'>
         <p className='line-clamp-2 sm:line-clamp-3'>{content}</p>
-        <div className='flex flex-col sm:flex-row sm:items-center gap-2 sm:gap-8'>
+        <div className='flex flex-col sm:flex-row items-end sm:items-center gap-2 sm:gap-8'>
           <Button className={'w-fit' + ButtonColorClasses[appType.buttonBackgroundState]}><Sparkles className='mr-2' />完了にする</Button>
           <Link href={'#'} className='underline'>
             元のメッセージを見る
