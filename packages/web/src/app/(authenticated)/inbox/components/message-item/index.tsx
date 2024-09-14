f7dde5b2f6f0619064eb3b20feb74caa2b817697
nonrita <2220025@ecc.ac.jp>
2024-09-15 02:08:48 +0900

    change Propsの方をMessageに変更

--- packages/web/src/app/(authenticated)/inbox/components/message-item/index.tsx
+++ packages/web/src/app/(authenticated)/inbox/components/message-item/index.tsx
@@ -1,27 +1,16 @@
 import { Sparkles } from 'lucide-react';
 import { NextPage } from 'next';
-import { StaticImport } from 'next/dist/shared/lib/get-img-props';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '~/components/ui/button';
 import DiscordImg from '../../../../../../public/app-logo/discord.svg';
 import Account from './account';
-import { AppProps, AppsProps, AppType, ButtonColorClasses, ButtonState } from './app-type';
+import { AppProps, AppsProps, ButtonColorClasses, defaultAppProps } from './app-type';
 import Label from './label';
-import { LabelType } from './label-type';
+import { Message } from '~/libs/types/message';
 
-type Props = {
-  app: AppType,
-  user: {
-    img: string | StaticImport,
-    name: string
-  },
-  message: string,
-  labelType: LabelType
-}
-
-const MessageItem: NextPage<Props> = ({app, user, message, labelType}) => {
-  const appType: AppProps = AppsProps[app]
+const MessageItem: NextPage<Message> = ({app, sender_image, sender_name, content, priority}) => {
+  const appType: AppProps = AppsProps[app] || defaultAppProps
 
   return (
     <div className={'flex flex-col gap-3 h-60 sm:h-56 w-full p-4 pr-8 border rounded-3xl relative overflow-hidden ' + appType.itemClass} >
@@ -34,13 +23,13 @@ const MessageItem: NextPage<Props> = ({app, user, message, labelType}) => {
       )}
       <div className='flex justify-between z-10'>
         <div className='flex gap-2 items-center'>
-          <Account appImg={appType.img || DiscordImg} user={user} />
-          <Label labelType={labelType}/>
+          <Account appImg={appType.img || DiscordImg} sender_image={sender_image} sender_name={sender_name} />
+          <Label priority={priority}/>
         </div>
         <p>00:00</p>
       </div>
       <div className='ml-8 flex-grow flex flex-col justify-between text-ellipsis overflow-hidden z-10'>
-        <p className='line-clamp-2 sm:line-clamp-3'>{message}</p>
+        <p className='line-clamp-2 sm:line-clamp-3'>{content}</p>
         <div className='flex flex-col sm:flex-row sm:items-center gap-2 sm:gap-8'>
           <Button className={'w-fit' + ButtonColorClasses[appType.buttonBackgroundState]}><Sparkles className='mr-2' />完了にする</Button>
           <Link href={'#'} className='underline'>
