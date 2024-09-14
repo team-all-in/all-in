c6081545eda90e172c7e69fa8656179139f971d8
RYU <takahashi.ryunosuke.job@gmail.com>
2024-09-14 22:42:54 +0900

    refactor(web): update setting page layout

--- packages/web/src/app/(authenticated)/settings/page.tsx
+++ packages/web/src/app/(authenticated)/settings/page.tsx
@@ -1,20 +1,15 @@
-import DiscordButton from './components/DiscordButton';
-import GithubButton from './components/GithubButton';
-import SignoutButton from './components/SignoutButton';
-import SlackButton from './components/SlackButton';
+import AppSettings from './components/app';
+import BackLink from './components/back-link';
+import UserSettings from './components/user';
 
 export default async function Settings() {
   return (
-    <div className='flex h-screen w-screen items-center justify-center'>
-      <div className='my-5 w-[28%] items-center'>
-        <p className='mx-8 mb-10 ml-10 font-bold text-black'>Solidarity setting</p>
-        <div className='flex justify-center'>
-          <DiscordButton />
-          <GithubButton />
-          <SlackButton />
-        </div>
-        <SignoutButton />
+    <article>
+      <BackLink />
+      <div className='space-y-3 px-3'>
+        <AppSettings />
+        <UserSettings />
       </div>
-    </div>
+    </article>
   );
 }
