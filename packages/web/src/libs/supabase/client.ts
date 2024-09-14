a9beb8e70f57bf333ea32371768637512fbf9609
RYU <takahashi.ryunosuke.job@gmail.com>
2024-09-14 12:31:03 +0900

    chore(web): init supabse auth

---
+++ packages/web/src/libs/supabase/client.ts
@@ -0,0 +1,12 @@
+import { createBrowserClient } from '@supabase/ssr';
+
+export function createClient() {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+  if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error('Supabase URL or Anon Key is not defined.');
+  }
+
+  return createBrowserClient(supabaseUrl, supabaseAnonKey);
+}
