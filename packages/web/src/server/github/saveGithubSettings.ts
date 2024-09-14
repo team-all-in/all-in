b804b63762548e1a6dae98b7d5ccf9a0dfce5e8d
hukurou0 <junjun.0712.on@gmail.com>
2024-09-15 08:28:14 +0900

    update if alredy exist

--- packages/web/src/server/github/saveGithubSettings.ts
+++ packages/web/src/server/github/saveGithubSettings.ts
@@ -12,9 +12,7 @@ export const saveGithubSettings = async (pat_token: string) => {
     console.error('user not found');
     return;
   }
-
-  // TODO: pat_tokenを暗号化する
-  // https://zenn.dev/nixo/articles/14a6ad953ea2db
+  
   const crypto = require('node:crypto');
   const cipher = crypto.createCipher('aes-256-cbc', 'allinpassword');
   const crypted = cipher.update(pat_token, 'utf-8', 'hex');
@@ -23,6 +21,28 @@ export const saveGithubSettings = async (pat_token: string) => {
   console.log(encrypt_pat_token);
 
   const supabase = createClient();
+  const { data: github_settings, error: github_settings_error } = await supabase
+    .from('github_settings')
+    .select('*')
+    .eq('user_id', user.id);
+  if (github_settings_error) {
+    console.error('Error fetching from Supabase:', github_settings_error);
+    return;
+  }
+  // 既に登録されている場合は更新
+  if (github_settings.length > 0) {
+    const { data, error } = await supabase
+      .from('github_settings')
+      .update({ encrypt_pat_token: encrypt_pat_token })
+      .eq('user_id', user.id);
+    if (error) {
+      console.error('Error updating to Supabase:', error);
+    } else {
+      console.log('Token updated to Supabase:', data);
+    }
+    return;
+  }
+  // 新規登録
   const { data, error } = await supabase
     .from('github_settings')
     .insert([{ user_id: user.id, encrypt_pat_token: encrypt_pat_token }]);
@@ -32,8 +52,6 @@ export const saveGithubSettings = async (pat_token: string) => {
   } else {
     console.log('Token saved to Supabase:', data);
   }
-
-  // TODO: encrypt_pat_tokenをsupabaseに保存する
 
   return;
 };
