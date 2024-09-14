e04557bb1a700ec7f53f79dec9822c5229e09528
nonrita <2220025@ecc.ac.jp>
2024-09-15 00:58:55 +0900

    add accountコンポーネントを追加

--- packages/web/src/app/(authenticated)/inbox/components/message-item/account.tsx
+++ packages/web/src/app/(authenticated)/inbox/components/message-item/account.tsx
@@ -1,26 +1,38 @@
 import Image from 'next/image';
 import ALLImg from '../../../../../../public/all-in.svg'
 import DiscordImg from '../../../../../../public/app-logo/discord.svg'
+import { StaticImport } from 'next/dist/shared/lib/get-img-props';
+import { NextPage } from 'next';
 
-export default function Account() {
+type Props = {
+  appImg: string | StaticImport,
+  user: {
+    img: string | StaticImport,
+    name: string
+  }
+}
+
+const Account:NextPage<Props> = ({appImg, user}) => {
   return (
     <div className='flex gap-4 items-center'>
       <div className='relative'>
         <div className='flex items-center justify-center h-12 aspect-square rounded-full bg-white overflow-hidden border-2 border-gray-200'>
           <Image
-            src={ALLImg}
+            src={user.img}
             alt='tool-icon'
           />
         </div>
         <div className='absolute -right-2 -bottom-2 h-7 aspect-square rounded-full flex items-center justify-center overflow-hidden bg-white border-2 border-gray-200'>
           <Image
-            src={DiscordImg}
+            src={appImg}
             alt='tool-icon'
             className='h-4'
           />
         </div>
       </div>
-      <p>natanuki</p>
+      <p>{user.name}</p>
     </div>
   )
 }
+
+export default Account
