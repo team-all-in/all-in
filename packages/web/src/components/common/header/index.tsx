4517cf757ed2e7a6d3758121b7d612b7c29f43a9
nonrita <2220025@ecc.ac.jp>
2024-09-14 16:49:23 +0900

    change headerを固定し、headerの高さを外部に公開しました

--- packages/web/src/components/common/header/index.tsx
+++ packages/web/src/components/common/header/index.tsx
@@ -1,7 +1,33 @@
+import Image from "next/image";
+import { Button } from "~/components/ui/button";
+import LogoImg from "../../../../assets/logo.png";
+import SettingIcon from "../../../../assets/setting-icon.png"
+import Link from "next/link";
+
+export const headerHeight = 56
+
 export default function Header({}) {
+  const style = {
+    height: headerHeight
+  }
+
   return (
-    <header className="border border-red-500 bg-red-500/30 backdrop-blur fixed top-0 w-full p-2 z-40">
-      header
+    <header className="flex items-center justify-between border-b bg-white backdrop-blur  py-2 px-4 z-40" style={style}>
+      <Image
+        src={LogoImg}
+        alt="ALL-INのロゴ"
+      />
+
+      <nav className=" flex items-center">
+        <Button variant="ghost" size="icon">
+          <Link href="/settings">
+            <Image
+              src={SettingIcon}
+              alt="settingのicon"
+            />
+          </Link>
+        </Button>
+      </nav>
     </header>
   );
 }
