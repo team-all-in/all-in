a44ffdd5acfd64c2f4def86c9fcfc172b0f31eed
nonrita <2220025@ecc.ac.jp>
2024-09-14 16:55:38 +0900

    add Filterを追加しました

--- packages/web/src/app/(authenticated)/inbox/page.tsx
+++ packages/web/src/app/(authenticated)/inbox/page.tsx
@@ -1,37 +1,42 @@
+import Filter from "~/components/common/filter";
+
 export default async function Inbox() {
   return (
-    <div className="p-3 pt-32 sm:pt-14 h-dvh overflow-y-auto space-y-2">
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
-      <div className="w-full border h-10"></div>
+    <div className="flex h-full flex-col sm:flex-row">
+      <Filter />
+      <div className="p-3 w-full h-full overflow-auto space-y-2">
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+        <div className="w-full border h-10"></div>
+      </div>
     </div>
   );
 }
