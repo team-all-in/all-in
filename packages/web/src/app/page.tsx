566cda0ff79b2ed7db65c4d8814f2c3f39630c59
RYU <takahashi.ryunosuke.job@gmail.com>
2024-09-14 14:37:10 +0900

    refactor(web): format

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -1,15 +1,14 @@
-import Image from "next/image";
-import { signInWithGoogle } from "~/server/actions/auth";
+import { signInWithGoogle } from '~/server/actions/auth';
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className='grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20'>
+      <main className='row-start-2 flex flex-col items-center gap-8 sm:items-start'>
         <form action={signInWithGoogle}>
-          <button>sign in with google</button>
+          <button type='submit'>sign in with google</button>
         </form>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+      <footer className='row-start-3 flex flex-wrap items-center justify-center gap-6' />
     </div>
   );
 }
