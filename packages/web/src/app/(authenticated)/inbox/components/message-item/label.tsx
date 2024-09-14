e32c2517f30d33a32501712c1ce1753e02153bfb
nonrita <2220025@ecc.ac.jp>
2024-09-14 20:40:55 +0900

    add labelの要素の作成

---
+++ packages/web/src/app/(authenticated)/inbox/components/message-item/label.tsx
@@ -0,0 +1,8 @@
+
+export default function Label() {
+  return (
+    <div className='py-1 px-4 h-fit w-fit bg-white bg-opacity-40 border-2 border-yellow-400 text-yellow-400 rounded-full'>
+      medium
+    </div>
+  )
+}
