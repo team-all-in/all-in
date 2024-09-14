28c249f79690ccdab54462cd9bacd2b33ee3a890
nonrita <2220025@ecc.ac.jp>
2024-09-15 02:48:13 +0900

    add 日付のUIを作成しました

--- packages/web/src/app/(authenticated)/inbox/page.tsx
+++ packages/web/src/app/(authenticated)/inbox/page.tsx
@@ -7,9 +7,15 @@ export default async function Inbox() {
 
   return (
     <div className='h-dvh space-y-2 overflow-y-auto p-3 pt-32 sm:pt-14'>
-      {messages?.map(message => (
-        <MessageItem {...message} />
-      ))}
+      <div className='flex'>
+        <div className='bg-black flex-grow w-4 rounded-full'/>
+        <div className='p-3 w-full flex flex-col gap-4'>
+          <h2 className='text-xl font-bold'>2024/09/15</h2>
+          {messages?.map(message => (
+            <MessageItem {...message} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
