1e1899fc9ce0c3b820bb60c07b8a5f11efe664c7
nonrita <2220025@ecc.ac.jp>
2024-09-16 04:15:53 +0900

    feat(web): add template

--- packages/web/src/app/layout.tsx
+++ packages/web/src/app/layout.tsx
@@ -2,11 +2,14 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'ALL-IN',
+  title: {
+    template: 'ALL IN - %s',
+    default: 'ホーム'
+  },
   description: 'すべての会話を、ひとつの場所に。',
   keywords: ['通知', '一覧', 'アプリケーション', '便利', 'メッセージ'],
   openGraph: {
-    title: 'ALL-IN',
+    title: 'ALL IN',
     description: 'すべての会話を、ひとつの場所に。',
     url: 'https://all-in-henna.vercel.app/inbox',
     type: 'website',
