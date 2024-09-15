41b2761d4537d2c98da7f70fc0a8bb068afacf54
momominamihinata <chocohina.4673@gmail.com>
2024-09-15 21:45:34 +0900

    refactor(web): 環境変数とteam

--- packages/web/src/app/(authenticated)/settings/slack/page.tsx
+++ packages/web/src/app/(authenticated)/settings/slack/page.tsx
@@ -4,15 +4,15 @@ export default function Slack() {
   const url = 'https://slack.com/oauth/v2/authorize?';
 
   const client_id = process.env.NEXT_PUBLIC_SLACK_CLIENT_ID || '';
-  const clientSecret = process.env.NEXT_PUBLIC_SLACK_CLIENT_SECRET || '';
   const redirect_uri = `${process.env.NEXT_PUBLIC_APP_URL}/oauth/slack`;
   const response_type = 'code';
   const scope =
     'channels:history,channels:join,channels:read,chat:write,groups:history,groups:read,users:read';
+  const team = 'T07MK5E7A68';
 
   const params = new URLSearchParams({
     client_id,
-    clientSecret,
+    team,
     redirect_uri,
     response_type,
     scope,
