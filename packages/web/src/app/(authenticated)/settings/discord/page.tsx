561b9ab4516a7d9db9368d1f93ee743d5b109192
RYU <takahashi.ryunosuke.job@gmail.com>
2024-09-15 14:47:58 +0900

    refactor(web): update discord oauth redirect

--- packages/web/src/app/(authenticated)/settings/discord/page.tsx
+++ packages/web/src/app/(authenticated)/settings/discord/page.tsx
@@ -1,21 +1,21 @@
-'use client'
-
-import {useEffect } from "react";
+import { redirect } from 'next/navigation';
 
 export default function Discord() {
-  const discordClientId = process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID;
-  const discordRedirectUri = process.env.NEXT_PUBLIC_DISCORD_REDIRECT_URI;
-  const discordOAuthUrl = `https://discord.com/oauth2/authorize?client_id=${discordClientId}&redirect_uri=${discordRedirectUri}&response_type=code&scope=identify%20email`;
+  const url = 'https://discord.com/oauth2/authorize?';
 
-  const discordConnect = () => {
-    window.location.href = discordOAuthUrl;
+  const client_id = process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID || '';
+  const redirect_uri = process.env.NEXT_PUBLIC_DISCORD_REDIRECT_URI || '';
+  const response_type = 'code';
+  const scope = 'identify email';
 
-  };
+  const params = new URLSearchParams({
+    client_id,
+    redirect_uri,
+    response_type,
+    scope,
+  });
 
-  useEffect(() => {
-    discordConnect();
-  }
-  , []);
+  const discordOAuthUrl = `${url}${params}`;
 
-  return <div>Discord</div>;
+  redirect(discordOAuthUrl);
 }
