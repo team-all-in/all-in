42bbd6a8c9e3c11d404711c10b79e349d0891b60
nakano1122 <akito2998@gmail.com>
2024-09-16 01:29:41 +0900

    DBに格納完了

--- packages/discord/discordAPI.py
+++ packages/discord/discordAPI.py
@@ -2,11 +2,29 @@ from app_setting import supabase_client
 import discord
 import os
 import logging
+import requests
+from fastapi import FastAPI
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+api = FastAPI()
+
+@api.get("/")
+def health():
+    return {"health": "ok"}
+
 client = discord.Client(intents=discord.Intents.all())
+
+headers = {
+    'Authorization': f'Bot {os.getenv("DISCORD_BOT_TOKEN")}',
+}
+
+def get_priority_and_sentient(message_text):
+    response = requests.get(
+        f"https://kctebirgsq.ap-northeast-1.awsapprunner.com/predict?text={message_text}"
+    ).json()
+    return response["sentiment"], response["priority"]
 
 # 自分がメンションされたメッセージを取得してDBに格納する関数
 @client.event
@@ -14,24 +32,31 @@ async def on_message(message):
     # supabaseに入っているユーザの一覧を取得
     supabase_users = (
         supabase_client.table("all-in-relation")
-        .select("discord_member_id")
+        .select("discord_member_id", "user_id")
         .execute()
     ).data
     supabase_user_name_list = [user["discord_member_id"] for user in supabase_users]
     # logger.info(f"{message.mentions}")
     # 取得したメッセージのmentionsがsupabaseに入っていればDBに格納する
+    messages = []
     for mention in message.mentions:
         if mention.name in supabase_user_name_list:
-            message = {
-                "user_id": mention.name,
+            url = f'''https://discord.com/api/v10/channels/{message.channel.id}/messages/{message.id}'''
+            content = requests.get(url, headers=headers).json()["content"]
+            sentiment, priority = get_priority_and_sentient(content)
+            supabase_user = [user for user in supabase_users if user["discord_member_id"] == mention.name][0]
+            messages.append({
+                "user_id": supabase_user["user_id"],
                 "app": "discord",
                 "server_id": message.guild.id,
                 "message_id": message.id,
                 "channel_id": message.channel.id,
-                "sentiment": "neutral",
-                "priority": "low",
-            }
-            supabase_client.table("messages").insert(message).execute()
+                "sentiment": sentiment,
+                "priority": priority,
+            })
+        else:
+            continue
+    supabase_client.table("messages").insert(messages).execute()
 
 if __name__ == "__main__":
     client.run(os.getenv("DISCORD_BOT_TOKEN"))
