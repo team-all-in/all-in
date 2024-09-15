cae2a1bd6c1f32f877540181c1a166b5dd2d67df
nonrita <2220025@ecc.ac.jp>
2024-09-16 04:23:08 +0900

    feat(web): add metadata

--- packages/web/src/app/(authenticated)/settings/page.tsx
+++ packages/web/src/app/(authenticated)/settings/page.tsx
@@ -1,6 +1,11 @@
+import { Metadata } from 'next';
 import AppSettings from './components/app';
 import BackLink from './components/back-link';
 import UserSettings from './components/user';
+
+export const metadata: Metadata = {
+  title: '設定',
+};
 
 export default async function Settings() {
   return (
