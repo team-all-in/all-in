04e1c0b0597d81401482d043be2055b6fecd6a51
nakano1122 <akito2998@gmail.com>
2024-09-15 17:01:03 +0900

    Discord APIを使ったメッセージデータの取得

--- packages/api/src/discordAPI.py
+++ packages/api/src/discordAPI.py
@@ -1,19 +1,25 @@
+from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from app_setting import app, supabase_client, get_current_user
+from app_setting import app
 import discord
 # from discord import app_commands
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 import os
 from supabase import create_client, Client
-import datetime
+import requests
+
+load_dotenv(find_dotenv())
 
 url: str = os.getenv("SUPABASE_URL")
 key: str = os.getenv("SUPABASE_ANON_KEY")
 
 supabase: Client = create_client(url, key)
 
-client = discord.Client(intents=discord.Intents.default())
-load_dotenv()
+client = discord.Client(intents=discord.Intents.all())
+
+headers = {
+    'Authorization': f'Bot {os.getenv("DISCORD_BOT_TOKEN")}',
+}
 
 priority_rule = {
     "low": 30,
@@ -23,42 +29,57 @@ priority_rule = {
     "critical": 0
 }
 
-@client.event
-async def on_ready():
-    print('Bot is ready')
+# 自分がメンションされたメッセージを取得してDBに格納する関数
+# @client.event
+# async def on_message(message):
+#     # supabaseに入っているユーザの一覧を取得
+#     supabase_users_name = (
+#         supabase.table("user")
+#         .select("name")
+#         .execute()
+#     )
+#     # 取得したメッセージのmentionsがsupabaseに入っていればDBに格納する
+#     for mention in message.mentions:
+#         if mention in supabase_users_name:
+#             message = {
+#                 "app": "discord",
+#                 "server_id": message.guild.id,
+#                 "message_id": message.id,
+#                 "channel_id": message.channel.id,
+#                 "sentiment": "neutral",
+#                 "priority": "low",
+#             }
+#             supabase.table("message").insert(message).execute()
 
-@app.get("/redirect")
-async def listen_code(code: str = ""):
-    return {"code: ": code}
+# @app.post("/get_messages")
+def get_messages() -> dict:
+    # app = requests["app"]
+    app = "discord"
+    if app != "discord":
+        return JSONResponse(status_code=400, content={"error": "app is not discord"})
+    # guild_id = requests["server_id"]
+    # message_id = requests["message_id"]
+    # channel_id = requests["channel_id"]
+    channel_id = 1275808876750700640
+    message_id = 1284766126621982781
+    url = f'https://discord.com/api/v10/channels/{channel_id}/messages/{message_id}'
+    message = requests.get(url, headers=headers).json()
+    response = {
+        "id": message["id"],
+        "app": "discord",
+        "sender_image": message["author"]["avatar"],
+        "sender_name": message["author"].get("username"),
+        # "server_name": message["guild_id"],
+        "content": message["content"],
+        "message_link": message["attachments"],
+        "send_at": message["edited_timestamp"],
+    }
+    response["send_at"] = message["edited_timestamp"] if message["edited_timestamp"] else message["timestamp"]
+    print(response)
+    return JSONResponse(status_code=200, content=response)
 
-# メッセージを取得する関数
-@app.get("/fetch_messages")
-async def fetch_messages(userId: int, code: str) -> dict:
-    discord_settings = supabase.table("discord_settings").select("*").eq("user_id", userId).execute()
-    user = supabase.table("users").select("*").eq("id", userId).execute()
-    messages = []
-    for message in discord.message.Message:
-        if message.mentions.find(user.name) or discord.message.mention_everyone:
-            # 優先度の基準によって変化する
-            if datetime.datetime.today() - message.created_at < priority_rule["critical"]:
-                priority = "critical"
-            elif datetime.datetime.today() - message.created_at < priority_rule["urgent"]:
-                priority = "urgent"
-            elif datetime.datetime.today() - message.created_at < priority_rule["high"]:
-                priority = "high"
-            elif datetime.datetime.today() - message.created_at < priority_rule["medium"]:
-                priority = "medium"
-            elif datetime.datetime.today() - message.created_at < priority_rule["low"]:
-                priority = "low"
-            messages.append({
-                "channel": message.channel,
-                "sender_name": message.author,
-                "content": message.content,
-                "message_link": message.jump_url,
-                "sentiment": message.emoji,
-                "priority": priority,
-                "send_at": message.edited_at | message.created_at
-            })
-    messages.sort(key=lambda x: x["send_at"], reverse=True)
-    # 上位20件を返す
-    return JSONResponse(content={"messages": messages[0:20]})
+response = get_messages()
+print(response)
+
+# if __name__ == "__main__":
+#     client.run(os.getenv("DISCORD_ACCESSTOKEN"))
