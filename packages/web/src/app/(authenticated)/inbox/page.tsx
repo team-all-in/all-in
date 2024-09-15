a89c547700b56b3551844243e517086b9bdd354e
RYU <takahashi.ryunosuke.job@gmail.com>
2024-09-15 14:47:20 +0900

    refactor(web): format

--- packages/web/src/app/(authenticated)/inbox/page.tsx
+++ packages/web/src/app/(authenticated)/inbox/page.tsx
@@ -4,10 +4,10 @@ import { getMessages } from './data/getMessages';
 import { groupMessagesByDate } from './data/groupMessagesByDate';
 
 export default async function Inbox() {
-  const messages = await getMessages()
-  const sortedMessages = messages?.sort((a, b) => dayjs(b.send_at).unix() - dayjs(a.send_at).unix()) || [];
-  const groupedMessages = (messages ? groupMessagesByDate(sortedMessages) : {});
-  console.log('sortedMessages', sortedMessages);
+  const messages = await getMessages();
+  const sortedMessages =
+    messages?.sort((a, b) => dayjs(b.send_at).unix() - dayjs(a.send_at).unix()) || [];
+  const groupedMessages = messages ? groupMessagesByDate(sortedMessages) : {};
 
   return (
     <div className='h-dvh space-y-8 overflow-y-auto p-3 pt-32 sm:pt-14'>
@@ -16,12 +16,12 @@ export default async function Inbox() {
           <div className='w-4 flex-grow rounded-full bg-black' />
           <div className='flex w-full flex-col gap-4 p-3'>
             <h2 className='font-bold text-xl'>{date}</h2>
-            {messages.map((message) => (
+            {messages.map(message => (
               <MessageItem key={message.id} {...message} />
             ))}
           </div>
         </div>
-    ))}
+      ))}
     </div>
   );
 }
