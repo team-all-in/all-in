f008b54bc5737d5d449fe88ca2ba0ababacfdb15
momominamihinata <chocohina.4673@gmail.com>
2024-09-15 20:11:13 +0900

    feat: checkGithubSettings,checkSlackSettings

--- packages/web/src/app/(authenticated)/settings/components/app/SlackButton.tsx
+++ packages/web/src/app/(authenticated)/settings/components/app/SlackButton.tsx
@@ -1,13 +1,18 @@
 import Link from 'next/link';
 import { Button } from '~/components/ui/button';
+import { checkSlackSettings } from '../../data/get-app-settings';
+import EnableLabel from './enable-label';
 
-export default function SlackButton() {
+export default async function SlackButton() {
+  const isEnabled = await checkSlackSettings();
+
   return (
     <div className='mx-4 flex-col'>
       <div className='flex justify-center'>
         <Link href='/settings/slack'>
           <Button variant='slack' size='tools' />
         </Link>
+        {isEnabled && <EnableLabel />}
       </div>
       <div className='items-center text-center font-bold text-sm'>slack</div>
     </div>
