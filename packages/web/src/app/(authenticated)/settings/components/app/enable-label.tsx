99ee008624708a98a510fd62ccc44c53b0facfc6
RYU <takahashi.ryunosuke.job@gmail.com>
2024-09-15 22:52:03 +0900

    refactor(web): update label style

--- packages/web/src/app/(authenticated)/settings/components/app/enable-label.tsx
+++ packages/web/src/app/(authenticated)/settings/components/app/enable-label.tsx
@@ -3,7 +3,7 @@ import { Badge } from '~/components/ui/badge';
 
 export default function EnableLabel() {
   return (
-    <Badge className='-top-1 -right-1 absolute border-green-500 bg-card p-1 text-green-500'>
+    <Badge className='-top-1 -right-1 absolute border-green-500 bg-card p-1 text-green-500 hover:bg-card'>
       <Check strokeWidth={2.5} size={16} />
     </Badge>
   );
