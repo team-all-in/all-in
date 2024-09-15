297da9807137f4964c83756f1b0d77fb971a68ba
nonrita <2220025@ecc.ac.jp>
2024-09-16 04:01:50 +0900

    change(web): メタデータを移動しました

--- packages/web/src/app/(authenticated)/inbox/page.tsx
+++ packages/web/src/app/(authenticated)/inbox/page.tsx
@@ -2,18 +2,6 @@ import { Suspense } from 'react';
 import MessageList from './components/message-list';
 import { getMessages } from './data/getMessages';
 
-export const metadata = {
-  title: 'ALL-IN メッセージ一覧',
-  description: '登録しているアプリケーションの通知が一覧で見れます',
-  keywords: ['通知', '一覧', 'アプリケーション', '便利'],
-  openGraph: {
-    title: 'ALL-IN メッセージ一覧',
-    description: '登録しているアプリケーションの通知が一覧で見れます',
-    url: 'https://all-in-henna.vercel.app/inbox',
-    type: 'website',
-  },
-}
-
 export default async function Inbox() {
   const messages = await getMessages();
 
