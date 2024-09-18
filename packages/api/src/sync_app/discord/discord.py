e283a8cdba4cd0781addb3a49488d47814034849
naruse666 <0x706f6b6f@gmail.com>
2024-09-19 03:45:04 +0900

    refactor: discord logging

--- packages/api/src/sync_app/discord/discord.py
+++ packages/api/src/sync_app/discord/discord.py
@@ -1,12 +1,16 @@
+from fastapi.responses import JSONResponse
+import discord
+from src.logger import newLogger
+from src.sync_app.discord.discord_api import discord_api_url
+from dotenv import load_dotenv, find_dotenv
 import os
 
-import discord
 import requests
-from dotenv import find_dotenv, load_dotenv
-from fastapi.responses import JSONResponse
 from supabase import Client, create_client
+from requests.exceptions import HTTPError
 
 load_dotenv(find_dotenv())
+logger = newLogger('discord')
 
 url: str = os.getenv("SUPABASE_URL")
 key: str = os.getenv("SUPABASE_ANON_KEY")
@@ -31,21 +35,28 @@ priority_rule = {
 def get_discord_message(
     server_id: str, channel_id: str, message_id: str
 ) -> dict:
-    url = f'''https://discord.com/api/v10/channels/{
-        channel_id}/messages/{message_id}'''
-    message = requests.get(url, headers=headers).json()
+    url = f"{discord_api_url}/channels/{channel_id}/messages/{message_id}"
+    try:
+        message = requests.get(url, headers=headers).json()
+        # raise exception when response code in 4XX, 5XX
+        message.raise_for_status()
+    except HTTPError as e:
+        logger.error(f"failed to get message: {e}")
+        raise e
 
-    response = {
-        "id": message_id,
+    response = new_response(message)
+    response["send_at"] = message["edited_timestamp"] if message["edited_timestamp"] else message["timestamp"]
+
+    return JSONResponse(status_code=200, content=response)
+
+
+def new_response(message: dict) -> dict:
+    return {
+        "id": message["id"],
         "app": "discord",
-        "sender_image": f"https://cdn.discordapp.com/avatars/{message['author']['id']}/{message['author']['avatar']}",
-        "sender_name": message["author"]["global_name"],
-        "server_image": "",  # 後で実装. 別のAPIで取得する
-        "server_name": "",  # 後で実装. 別のAPIで取得する
-        "channel_name": "",  # 後で実装. 別のAPIで取得する
+        "sender_image": message["author"]["avatar"],
+        "sender_name": message["author"].get("username"),
         "content": message["content"],
-        "message_link": f"https://discord.com/channels/{server_id}/{channel_id}/{message_id}",
-        "send_at": message["timestamp"],
+        "message_link": message["attachments"],
+        "send_at": message["edited_timestamp"],
     }
-
-    return response
