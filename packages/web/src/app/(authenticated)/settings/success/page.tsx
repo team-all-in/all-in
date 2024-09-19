93e01734404a363c9e34f7b99c9d6c895d763f85
nonrita <2220025@ecc.ac.jp>
2024-09-20 00:19:15 +0900

    feat(web): appによってデザインが変わるように変更しました

--- packages/web/src/app/(authenticated)/settings/success/page.tsx
+++ packages/web/src/app/(authenticated)/settings/success/page.tsx
@@ -1,36 +1,55 @@
+"use client"
+
 import BackLink from "../components/back-link"
 import Image from "next/image"
-import DiscordImg from "./assets/discord.svg"
 import { Button, buttonVariants } from "~/components/ui/button"
 import Link from "next/link"
 import { ArrowRightIcon } from "lucide-react"
+import { useSearchParams } from "next/navigation"
+import AppType from "./appType"
 
 
 export default function Success() {
-  const appName = "Discrod"
+  const searchParams = useSearchParams();
+  const getAppNameParams = searchParams.get("appName")
+  const app = AppType.find((app) => app.type == getAppNameParams)
+
   return (
-    <div className="bg-indigo-600 h-screen -mt-14">
+    app ?
+    <div
+      className={`h-screen -mt-14 ${app.backgroundColorClass}`}
+    >
       <div className="pt-14 h-full relative">
         <BackLink
           href="/settings"
           place="設定"
-          textColor="white"
+          textColor={app.textColor}
         />
         <div className="flex flex-col items-center w-fit absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           <Image
-            src={DiscordImg}
+            src={app.img}
             alt="appIcon"
             height={320}
           />
-          <p className="text-5xl mb-8 text-white">{appName}との連携が完了しました</p>
-          <Button variant={"outline"}>
+          <p
+            className={`text-5xl mb-8 text-${app.textColor}`}
+          >
+            {app.name}との連携が完了しました
+          </p>
+          <Button variant={"outline"} className="bg-white">
             <Link href={"/"} className={buttonVariants({ variant: 'link' })}>
-              メッセージ一覧を見る
-              <ArrowRightIcon />
+              <span className="text-black">
+                メッセージ一覧を見る
+              </span>
+              <ArrowRightIcon color="black" />
             </Link>
           </Button>
         </div>
       </div>
+    </div>
+    :
+    <div>
+      <h1>認証されたアプリが見つかりません</h1>
     </div>
   )
 }
