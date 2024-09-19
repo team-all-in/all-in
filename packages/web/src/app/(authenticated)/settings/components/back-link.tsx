0a9c8ca594c3dc2fa02495179f08433553589623
nonrita <2220025@ecc.ac.jp>
2024-09-19 22:23:21 +0900

    change(web):　テキストの色を変えれるように変更しました

--- packages/web/src/app/(authenticated)/settings/components/back-link.tsx
+++ packages/web/src/app/(authenticated)/settings/components/back-link.tsx
@@ -1,18 +1,21 @@
 import { ArrowLeftIcon } from 'lucide-react';
 import Link from 'next/link';
+import { text } from 'stream/consumers';
 import { buttonVariants } from '~/components/ui/button';
 
 export default function BackLink({
   href = '/inbox',
   place,
+  textColor,
 }: {
   href?: string;
   place?: string;
+  textColor?: string;
 }) {
   return (
     <Link href={href} className={buttonVariants({ variant: 'link' })}>
-      <ArrowLeftIcon className='mr-2' size={18} />
-      <span>{place ? `${place}に` : ''}もどる</span>
+      <ArrowLeftIcon className='mr-2' size={18} color={textColor} />
+      <span className={`text-${textColor}`}>{place ? `${place}に` : ''}もどる</span>
     </Link>
   );
 }
