38b05f8af92918a36f88851fd815e59367460293
RYU <takahashi.ryunosuke.job@gmail.com>
2024-09-20 16:26:30 +0900

    feat(web): add message skelton

--- packages/web/src/app/(authenticated)/inbox/page.tsx
+++ packages/web/src/app/(authenticated)/inbox/page.tsx
@@ -1,19 +1,17 @@
 import type { Metadata } from 'next';
 import { Suspense } from 'react';
-import MessageList from './components/message-list';
-import { getMessages } from './data/getMessages';
+import Messages from './components/message';
+import MessageSkelton from './components/message/loading';
 
 export const metadata: Metadata = {
   title: 'メッセージ一覧',
 };
 
 export default async function Inbox() {
-  const messages = await getMessages();
-
   return (
     <div className='h-dvh space-y-4 overflow-y-auto p-3 pt-16'>
-      <Suspense>
-        <MessageList messages={messages} />
+      <Suspense fallback={<MessageSkelton />}>
+        <Messages />
       </Suspense>
     </div>
   );
