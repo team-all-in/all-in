5ec9fbb447a126e05759beb9f15e78e9644d890a
nonrita <2220025@ecc.ac.jp>
2024-09-20 22:36:54 +0900

    fix(web): labelのアイコンと色を変更しました

--- packages/web/src/app/(authenticated)/settings/components/app/enable-label.tsx
+++ packages/web/src/app/(authenticated)/settings/components/app/enable-label.tsx
@@ -1,4 +1,4 @@
-import { Check, CircleDashed } from 'lucide-react';
+import { Check, CircleCheck, CircleDashed } from 'lucide-react';
 import { Badge } from '~/components/ui/badge';
 
 type Props = {
@@ -10,12 +10,12 @@ export default function EnableLabel({isEnabled}: Props) {
     <>
       {isEnabled ?
         (
-          <Badge className='bg-green-400 border-white px-2 py-1.5 text-white flex gap-1 hover:bg-green-400'>
-            <Check strokeWidth={2.5} size={16} />
+          <Badge className='bg-green-500 border-white px-2 py-1.5 text-white flex gap-1 hover:bg-green-400'>
+            <CircleCheck strokeWidth={2.5} size={16} />
             連携済み
           </Badge>
         ):(
-          <Badge className='bg-gray-400 border-white px-2 py-1.5 text-white flex gap-1 hover:bg-gray-400'>
+          <Badge className='bg-gray-500 border-white px-2 py-1.5 text-white flex gap-1 hover:bg-gray-400'>
             <CircleDashed strokeWidth={2.5} size={16} />
             未連携
           </Badge>
