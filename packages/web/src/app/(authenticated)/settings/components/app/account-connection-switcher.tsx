367c3c41ef614433fa1a304d5b60261089823e76
nonrita <2220025@ecc.ac.jp>
2024-09-20 20:58:48 +0900

    feat(web): アカウントの連携状況によって表示するボタンを変えるコンポーネントを作成

---
+++ packages/web/src/app/(authenticated)/settings/components/app/account-connection-switcher.tsx
@@ -0,0 +1,44 @@
+import { RotateCw, Trash } from "lucide-react"
+import Link from "next/link"
+import { Button } from "~/components/ui/button"
+
+type Props = {
+  app: string
+  isEnabled: boolean
+}
+
+const AccountConnectionSwitcher = ({app, isEnabled}: Props) => {
+  return (
+    <>
+      {isEnabled ?
+        (
+          <div className='w-full flex flex-col sm:flex-row items-end sm:items-start gap-2'>
+            <Button className='w-fit'>
+              <Link href={""} className='flex items-center gap-1'>
+                <RotateCw />
+                再連携
+              </Link>
+            </Button>
+            <Button variant={"destructive"} className='w-fit'>
+              <Link href={""} className='flex items-center gap-1'>
+                <Trash/>
+                連携を削除する
+              </Link>
+            </Button>
+          </div>
+        ):
+        (
+          <div className='w-full flex flex-col items-end sm:items-start'>
+            <Button className='w-fit'>
+              <Link href={`/settings/${app}`}>
+                連携する
+              </Link>
+            </Button>
+          </div>
+        )
+      }
+    </>
+  )
+}
+
+export default AccountConnectionSwitcher
