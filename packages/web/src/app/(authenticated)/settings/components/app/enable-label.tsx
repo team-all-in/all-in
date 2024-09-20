130fb98034497907d01d5b1c3e633bbce65819ae
nonrita <2220025@ecc.ac.jp>
2024-09-20 21:18:53 +0900

    fix(web): delete hover design

--- packages/web/src/app/(authenticated)/settings/components/app/enable-label.tsx
+++ packages/web/src/app/(authenticated)/settings/components/app/enable-label.tsx
@@ -10,12 +10,12 @@ export default function EnableLabel({isEnabled}: Props) {
     <>
       {isEnabled ?
         (
-          <Badge className='bg-green-400 border-white px-2 py-1.5 text-white flex gap-1'>
+          <Badge className='bg-green-400 border-white px-2 py-1.5 text-white flex gap-1 hover:bg-green-400'>
             <Check strokeWidth={2.5} size={16} />
             連携済み
           </Badge>
         ):(
-          <Badge className='bg-gray-400 border-white px-2 py-1.5 text-white flex gap-1'>
+          <Badge className='bg-gray-400 border-white px-2 py-1.5 text-white flex gap-1 hover:bg-gray-400'>
             <CircleDashed strokeWidth={2.5} size={16} />
             未連携
           </Badge>
