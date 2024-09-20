41a97a38273da5a4de71429501124971cfeb1df5
nonrita <2220025@ecc.ac.jp>
2024-09-21 02:06:03 +0900

    feat(web): add delete settings action

---
+++ packages/web/src/app/(authenticated)/settings/actions/deleteSettings.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import deleteSettings from "~/server/settings/deleteSettings";
+
+
+const deleteSettingsAction = async (_previousState: unknown, formData: FormData) => {
+  const app: string = formData.get('app') as string;
+
+  await deleteSettings(app)
+
+  return '';
+}
+
+export default deleteSettingsAction;
