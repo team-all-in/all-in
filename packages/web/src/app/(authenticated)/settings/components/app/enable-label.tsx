de6947968ef7e40e6680b35510f0a6abc74dd035
nonrita <2220025@ecc.ac.jp>
2024-09-20 22:46:00 +0900

    change(web): labelが見やすいようにカラーを変更しました

--- packages/web/src/app/(authenticated)/settings/components/app/enable-label.tsx
+++ packages/web/src/app/(authenticated)/settings/components/app/enable-label.tsx
@@ -10,12 +10,12 @@ export default function EnableLabel({isEnabled}: Props) {
     <>
       {isEnabled ?
         (
-          <Badge className='bg-green-500 border-white px-2 py-1.5 text-white flex gap-1 hover:bg-green-400'>
+          <Badge className='bg-green-400 border-2 border-green-600 px-2 py-1.5 text-white flex gap-1 hover:bg-green-400'>
             <CircleCheck strokeWidth={2.5} size={16} />
             連携済み
           </Badge>
         ):(
-          <Badge className='bg-gray-500 border-white px-2 py-1.5 text-white flex gap-1 hover:bg-gray-400'>
+          <Badge className='bg-gray-400 border-2 border-gray-600 px-2 py-1.5 text-white flex gap-1 hover:bg-gray-400'>
             <CircleDashed strokeWidth={2.5} size={16} />
             未連携
           </Badge>
