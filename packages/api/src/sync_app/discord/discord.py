c499cf8b664b47008fd91adb0b5906c11e0a40dc
KorRyu3 <146335193+KorRyu3@users.noreply.github.com>
2024-09-21 16:09:03 +0900

    refactor(discord): 役割ごとに関数化

--- packages/api/src/sync_app/discord/discord.py
+++ packages/api/src/sync_app/discord/discord.py
@@ -11,26 +11,25 @@ load_dotenv()
 
 logger = newLogger('discord')
 
-headers = {
-    'Authorization': f'Bot {os.getenv("DISCORD_BOT_TOKEN")}',
-}
+def get_headers() -> dict:
+    return {
+        'Authorization': f'Bot {os.getenv("DISCORD_BOT_TOKEN")}',
+    }
 
-def get_discord_message(
-    server_id: str, channel_id: str, message_id: str
-) -> MessageResponse:
-    url = f"{discord_api_url}/channels/{channel_id}/messages/{message_id}"
+def fetch_message(url: str, headers: dict) -> dict:
     try:
         response = requests.get(url, headers=headers)
         # raise exception when response code in 4XX, 5XX
         response.raise_for_status()
     except HTTPError as e:
-        logger.error(f"failed to get message: {e}")
+        logger.error(f"failed to fetch message: {e}")
         raise e
 
-    message = response.json()
+    return response.json()
 
+def create_message_response(message: dict, server_id: str, channel_id: str) -> MessageResponse:
     return {
-        "id": message_id,
+        "id": message["id"],
         "app": "discord",
         "sender_image": f"https://cdn.discordapp.com/avatars/{message['author']['id']}/{message['author']['avatar']}",
         "sender_name": message["author"]["global_name"],
@@ -38,6 +37,16 @@ def get_discord_message(
         "server_name": "",  # 後で実装. 別のAPIで取得する
         "channel_name": "",  # 後で実装. 別のAPIで取得する
         "content": message["content"],
-        "message_link": f"https://discord.com/channels/{server_id}/{channel_id}/{message_id}",
+        "message_link": f"https://discord.com/channels/{server_id}/{channel_id}/{message['id']}",
         "send_at": message["timestamp"],
     }
+
+def get_discord_message(
+    server_id: str, channel_id: str, message_id: str
+) -> MessageResponse:
+    url = f"{discord_api_url}/channels/{channel_id}/messages/{message_id}"
+    headers = get_headers()
+
+    message = fetch_message(url, headers)
+
+    return create_message_response(message, server_id, channel_id)
