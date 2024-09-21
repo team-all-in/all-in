575e13307bcbe92879f899bc0ab37e9adafd1fa1
RYU <takahashi.ryunosuke.job@gmail.com>
2024-09-22 00:16:18 +0900

    test: test

--- packages/web/src/app/(authenticated)/inbox/data/filterMessageByApp.ts
+++ packages/web/src/app/(authenticated)/inbox/data/filterMessageByApp.ts
@@ -2,4 +2,5 @@ import type { Message } from '~/libs/types/message';
 
 export const filterMessagesByApp = (messages: Message[], app: string) => {
   return messages.filter(message => message.app === app);
+  // test
 };
