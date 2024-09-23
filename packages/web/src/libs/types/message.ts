c982c3d65157056b9245b28c8c4eeb6a681f2b4f
RYU <takahashi.ryunosuke.job@gmail.com>
2024-09-23 22:56:53 +0900

    refactor(web): update message type

--- packages/web/src/libs/types/message.ts
+++ packages/web/src/libs/types/message.ts
@@ -8,8 +8,8 @@ export type Message = {
   channel_name: string;
   content: string;
   message_link: string;
-  sentiment: string;
-  priority: 1 | 2 | 3 | 4 | 5;
+  sentiment?: string;
+  priority?: 1 | 2 | 3 | 4 | 5;
   send_at: string;
 };
 
