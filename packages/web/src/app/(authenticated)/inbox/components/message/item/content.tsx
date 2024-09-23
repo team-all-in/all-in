5df4eeb81ea186435562d174067f37d47e944741
RYU <takahashi.ryunosuke.job@gmail.com>
2024-09-23 22:53:55 +0900

    refactor(web): format message-item content comp

--- packages/web/src/app/(authenticated)/inbox/components/message/item/content.tsx
+++ packages/web/src/app/(authenticated)/inbox/components/message/item/content.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
-export default function Content({ content }: { content: string }) {
+export default function MessageItemContent({ content }: { content: string }) {
   return <p className='line-clamp-2 break-all'>{content}</p>;
 }
