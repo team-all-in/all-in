fdd99766971caac66864c25e90bfd8e7118b41cd
RYU <takahashi.ryunosuke.job@gmail.com>
2024-09-23 13:24:17 +0900

    refactor(web): update header style

--- packages/web/src/components/common/header/index.tsx
+++ packages/web/src/components/common/header/index.tsx
@@ -2,7 +2,7 @@ import Logo from './logo';
 
 export default function Header() {
   return (
-    <header className='fixed top-0 z-40 w-full border-b bg-card/10 p-4 backdrop-blur'>
+    <header className='fixed top-0 z-40 flex w-full border-b bg-card/10 p-4 backdrop-blur'>
       <Logo />
     </header>
   );
