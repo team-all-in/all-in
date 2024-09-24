fa2a22cbf98e78c4d02e8e67d16107eef7e3cd42
RYU <takahashi.ryunosuke.job@gmail.com>
2024-09-24 14:25:44 +0900

    refactor(web): update message item content

--- packages/web/src/app/(authenticated)/inbox/components/message/item/content.tsx
+++ packages/web/src/app/(authenticated)/inbox/components/message/item/content.tsx
@@ -1,3 +1,28 @@
-export default function MessageItemContent({ content }: { content: string }) {
-  return <p className='line-clamp-2 break-all'>{content}</p>;
+export default function MessageItemContent({
+  content,
+  message_link,
+}: {
+  content: string;
+  message_link: string;
+}) {
+  if (!content) {
+    return (
+      <div className='text-red-500 text-sm'>
+        <p>メッセージを取得できませんでした。</p>
+        <div>
+          <a
+            href={message_link}
+            target='_blank'
+            rel='noreferrer'
+            className='cursor-pointer underline underline-offset-4'
+          >
+            元のメッセージ
+          </a>
+          <span>を確認してください。</span>
+        </div>
+      </div>
+    );
+  }
+
+  return <p className='line-clamp-2 break-all text-sm'>{content}</p>;
 }
