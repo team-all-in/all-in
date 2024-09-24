9742460ce7d58454918a7e9555c891060fed6b1a
KorRyu3 <146335193+KorRyu3@users.noreply.github.com>
2024-09-25 00:43:57 +0900

    feat(api): 絵文字プロンプトの変更

--- packages/api/src/predict/emotion/emotion.py
+++ packages/api/src/predict/emotion/emotion.py
@@ -9,6 +9,19 @@ load_dotenv()
 # OpenAI APIキーを設定
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+SYSTEM_PROMPT = """
+あなたは感情分析を行うアシスタントです。
+
+入力されたテキストに相当する絵文字を出力しなさい。
+**出力は一つの絵文字のみにすること。**
+
+### 入力例
+こんにちは
+
+### 出力例
+😊
+"""
+
 # 感情を分析する関数
 def analyze_emotion(input_text: str):
     try:
@@ -16,8 +29,8 @@ def analyze_emotion(input_text: str):
         response = openai.ChatCompletion.create(
             model="gpt-4o-mini",
             messages=[
-                {"role": "system", "content": "あなたは感情分析を行うアシスタントです。次のテキストを一つの絵文字（例: 😂）で教えてください"},
-                {"role": "user", "content": {input_text}}
+                {"role": "system", "content": SYSTEM_PROMPT},
+                {"role": "user", "content": input_text}
             ]
         )
 
