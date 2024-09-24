d9c4ec9fe6b6dbf4df476e9af4330ee2f6100542
KorRyu3 <146335193+KorRyu3@users.noreply.github.com>
2024-09-25 00:14:57 +0900

    style: fmt

--- packages/api/src/sync_app/discord/discordAPI.py
+++ packages/api/src/sync_app/discord/discordAPI.py
@@ -29,15 +29,23 @@ def fetch_api_response(url: str, headers: dict) -> dict:
 
 
 def create_message_response(
-    guild: dict, channel: dict, message: dict, server_id: str, channel_id: str, message_id: str
+    guild: dict,
+    channel: dict,
+    message: dict,
+    server_id: str,
+    channel_id: str,
+    message_id: str,
 ) -> MessageResponse:
     channel_name = channel["name"]
     sender = message["author"]
+    sender_name = (
+        sender["global_name"] if sender["global_name"] else sender["username"]
+    )
     return {
         "id": message_id,
         "app": "discord",
         "sender_image": f"https://cdn.discordapp.com/avatars/{sender['id']}/{sender['avatar']}",
-        "sender_name": sender["global_name"] if sender["global_name"] else sender["username"],
+        "sender_name": sender_name,
         "server_image": f"https://cdn.discordapp.com/icons/{guild['id']}/{guild['icon']}.png",
         "server_name": guild["name"],
         "channel_name": channel_name,
@@ -58,10 +66,14 @@ def get_discord_message(
     headers = get_headers(token)
     guild_url = f"{discord_api_url}/guilds/{server_id}"
     channel_url = f"{discord_api_url}/channels/{channel_id}"
-    message_url = f"{discord_api_url}/channels/{channel_id}/messages/{message_id}"
+    message_url = (
+        f"{discord_api_url}/channels/{channel_id}/messages/{message_id}"
+    )
 
     guild = fetch_api_response(guild_url, headers)
     channel = fetch_api_response(channel_url, headers)
     message = fetch_api_response(message_url, headers)
 
-    return create_message_response(guild, channel, message, server_id, channel_id, message_id)
+    return create_message_response(
+        guild, channel, message, server_id, channel_id, message_id
+    )
