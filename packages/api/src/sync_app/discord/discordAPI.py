965e4819c57c31f828a4fdabc978d0015ef4ce61
KorRyu3 <146335193+KorRyu3@users.noreply.github.com>
2024-09-25 00:13:11 +0900

    feat(discord): 冗長な箇所を関数にまとめる

--- packages/api/src/sync_app/discord/discordAPI.py
+++ packages/api/src/sync_app/discord/discordAPI.py
@@ -18,36 +18,26 @@ def get_headers(token: str) -> dict:
     }
 
 
-def fetch_message(url: str, headers: dict) -> dict:
+def fetch_api_response(url: str, headers: dict) -> dict:
     try:
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         return response.json()
     except HTTPError as e:
-        logger.error(f"failed to fetch message: {e}")
+        logger.error(f"failed to fetch data from {e}")
         raise
 
 
 def create_message_response(
-    message: dict, server_id: str, channel_id: str, message_id: str
+    guild: dict, channel: dict, message: dict, server_id: str, channel_id: str, message_id: str
 ) -> MessageResponse:
-    token = os.getenv("DISCORD_BOT_TOKEN")
-    headers = get_headers(token)
-    message = requests.get(
-        f"https://discord.com/api/v10/channels/{channel_id}/messages/{message_id}",
-        headers=headers,
-    ).json()
-    guild = requests.get(
-        f"https://discord.com/api/v10/guilds/{server_id}", headers=headers
-    ).json()
-    channel_name = requests.get(
-        f"https://discord.com/api/v10/channels/{channel_id}", headers=headers
-    ).json()["name"]
+    channel_name = channel["name"]
+    sender = message["author"]
     return {
         "id": message_id,
         "app": "discord",
-        "sender_image": f"https://cdn.discordapp.com/avatars/{message['author']['id']}/{message['author']['avatar']}",
-        "sender_name": message["author"]["global_name"],
+        "sender_image": f"https://cdn.discordapp.com/avatars/{sender['id']}/{sender['avatar']}",
+        "sender_name": sender["global_name"] if sender["global_name"] else sender["username"],
         "server_image": f"https://cdn.discordapp.com/icons/{guild['id']}/{guild['icon']}.png",
         "server_name": guild["name"],
         "channel_name": channel_name,
@@ -65,9 +55,13 @@ def get_discord_message(
         logger.error("DISCORD_BOT_TOKEN is not set")
         raise EnvironmentError("DISCORD_BOT_TOKEN is not set")
 
-    url = f"{discord_api_url}/channels/{channel_id}/messages/{message_id}"
     headers = get_headers(token)
+    guild_url = f"{discord_api_url}/guilds/{server_id}"
+    channel_url = f"{discord_api_url}/channels/{channel_id}"
+    message_url = f"{discord_api_url}/channels/{channel_id}/messages/{message_id}"
 
-    message = fetch_message(url, headers)
+    guild = fetch_api_response(guild_url, headers)
+    channel = fetch_api_response(channel_url, headers)
+    message = fetch_api_response(message_url, headers)
 
-    return create_message_response(message, server_id, channel_id, message_id)
+    return create_message_response(guild, channel, message, server_id, channel_id, message_id)
