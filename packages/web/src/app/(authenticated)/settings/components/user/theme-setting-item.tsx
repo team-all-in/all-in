e90e737fec535b31f56b8ec722055b35c8beec37
momominamihinata <chocohina.4673@gmail.com>
2024-09-26 18:09:00 +0900

    feat: themeSettingItem

---
+++ packages/web/src/app/(authenticated)/settings/components/user/theme-setting-item.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
+import React from 'react';
+
+interface Props {
+  theme: string;
+  label: string;
+  className: string;
+}
+
+export default function ThemeSettingItem({ theme, label, className }: Props) {
+  const { setTheme } = useTheme();
+  return (
+    <>
+      <div className={`${className}`}>
+        <Image src={`/theme/${theme}.svg`} alt='' width={150} height={150} />
+        <div className='mt-2 flex items-center'>
+          <input type='radio' name='theme' onClick={() => setTheme(theme)} />
+          <p className='mx-3 text-sm'>{label}</p>
+        </div>
+      </div>
+    </>
+  );
+}
