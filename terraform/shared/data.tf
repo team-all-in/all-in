64811288323fde2a325899210420fa8fc158009b
naruse (poko) <62323683+naruse666@users.noreply.github.com>
2024-10-15 19:02:44 +0900

    chore: close role (#293)

--- terraform/shared/data.tf
+++
@@ -1,7 +0,0 @@
-data "aws_ssm_parameter" "slack_bot_token" {
-  name = "slack_bot_token"
-}
-
-data "aws_ssm_parameter" "slack_refresh_token" {
-  name = "slack_refresh_token"
-}
--- terraform/shared/terraform.tf
+++ terraform/shared/terraform.tf
@@ -15,12 +15,3 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-data "terraform_remote_state" "all_in_env" {
-  backend = "s3"
-
-  config = {
-    bucket = "all-in-tfaction-backend"
-    key    = "terraform/env/v1/terraform.tfstate"
-    region = "ap-northeast-1"
-  }
-}
